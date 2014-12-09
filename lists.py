6bb0e13679be6f09bde9206a6d16363b701a27a5
damicore <damiangaggero@gmail.com>
2014-12-09 00:42:30 -0300

    Added exercise

--- lists.py
+++ lists.py
@@ -1,4 +1,24 @@
 #! /usr/bin/env python2
 
-def print_something(what):
-    print(what)
+def flatten_list(nested, newlist = []):
+    for element in nested:
+        if type(element) == list:
+            flatten_list(element, newlist)
+        else:
+            newlist.append(element)
+    return newlist
+
+def capitalize_all(t):
+    res = []
+    for s in t:
+        res.append(s.capitalize())
+    return res
+
+def only_upper(t):
+    res = []
+    for s in t:
+        if s.isupper():
+            res.append(s)
+    return res
+listai = [ 1, 2,[3, 4], 5, [6, [7, 8]]]
+lista_s = ['hola', 'QuiEn', [['sos'], 'Y']]
